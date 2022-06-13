cc1c58feb02eebb572c11eab89f43e67c6457012
Jeetendra chauhan <jeet4code@gmail.com>
2022-06-13 20:49:44 +0530

    change app component to standalone component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
 })
 export class AppComponent {
   title = 'myapp';
